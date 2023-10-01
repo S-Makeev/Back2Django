f241ea31c79a59f90c4ff663c8ef786f63025c3b
Slava Makeev <unclearnuclear@yahoo.com>
2023-10-01 01:13:39 +0000

    added anotheer view for CRUD

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 # Create your views here.
 
@@ -9,4 +11,11 @@ class TaskList(ListView):
       context_object_name = 'tasks'
 
 class TaskDetail(DetailView):
-      model = Task      
+      model = Task     
+      context_object_name = 'task' 
+      template_name = 'todo_app/task.html'
+
+class TaskCreate(CreateView):
+      model = Task
+      field = '__all__'
+      success_url = reverse_lazy('tasks')
